cd3f79949a27f1b4f362e61757a6fd2f8776aa39
Carson King <KingC18@MSLMI.gov>
2025-06-18 12:21:51 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,25 @@
+import requests
+
+import datetime
+
+def daily3():
+    print("Daily 3 selected")
+
+    yesterday = datetime.now() - timedelta(1)
+
+    print(datetime.strftime(yesterday, '%Y-%m-%d'))
+
+    url = "https://www.michiganlottery.com/games/daily-3?drawDate"
+
+    req = requests.get("https://www.michiganlottery.com/games/daily-3?drawDate=2025-06-17")
+
+print("Welcome to FastCheck! The slightly easier way to verify numbers on the MSL website")
+
+print("1 - Daily 3")
+
+input = int(input("What do you want to check? "))
+
+if(input == 1):
+    daily3()
+else:
+    print("no valid selections, DO BETTER")
