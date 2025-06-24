320d35122cd7e90a73a143f55bfe12e5d4415222
Carson King <KingC18@MSLMI.gov>
2025-06-24 09:11:18 -0400

    a

--- Main.py
+++ Main.py
@@ -1,26 +1,30 @@
-#import requests
+# import requests #ADD THIS BACK
 
-# INSTALL BEAUTIFUL SOUP https://beautiful-soup-4.readthedocs.io/en/latest/
+# INSTALL BEAUTIFUL SOUP FOR HTML PARSING https://beautiful-soup-4.readthedocs.io/en/latest/
 
 # INSTALL PDF PARSER FOR PDF PARSING https://www.freecodecamp.org/news/extract-data-from-pdf-files-with-python/
 
+# check daily 3 midday and evenings
 def daily3():
-    print("Daily 3 selected")
+    print("Daily 3 check started")
 
     url = "https://www.michiganlottery.com/games/daily-3"
 
+    # Stuff is commented out due to not having the module installed yet
 
-
-    #req = requests.get(url)
+    # req = requests.get(url)
 
     #print(req)
 
-print("Welcome to FastCheck! The slightly easier way to verify numbers on the MSL website")
+# Start of program
+print("Welcome to FastCheck! The program that saves you exactly 3 minutes of checking numbers")
 
 print("1 - Daily 3")
 
+# Get the input for which one to check, make it just simply check it in order later
 input = int(input("What do you want to check? "))
 
+# Keep this tree for now, check automatically in order later.
 if(input == 1):
     daily3()
 else:
